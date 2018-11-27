2735a2c544cdd8de69e452ebf5666fb694a40762
Kristijan Klindić <klindic94kristijan@gmail.com>
2018-11-27 23:50:32 +0100

    firebase init in module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,21 @@ import { AppComponent } from "./app.component";
 import { ItemsComponent } from "./item/items.component";
 import { ItemDetailComponent } from "./item/item-detail.component";
 
+import * as firebase from 'nativescript-plugin-firebase';
+
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
 
 // Uncomment and add to NgModule imports if you need to use the HttpClient wrapper
 // import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
+
+firebase
+    .init()
+    .then(() => {
+        console.log('Firebase initialized!');
+    }).catch(error => {
+        console.log('Firebase error: ', error);
+    })
 
 @NgModule({
     bootstrap: [
